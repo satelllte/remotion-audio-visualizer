fff265dda6ce8448bd0dbe406f1b745648ec6023
George Satellite <lunaerxs@gmail.com>
2023-06-24 21:56:40 +0300

    test error

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -3,7 +3,7 @@ import {AbsoluteFill, useCurrentFrame} from 'remotion';
 export const Composition = () => {
 	const frame = useCurrentFrame();
 	return (
-		<AbsoluteFill className='flex items-center justify-center bg-black text-4xl text-red-500'>
+		<AbsoluteFill className='flex items-center bg-black justify-center text-4xl text-red-500'>
 			{`F ${frame}`}
 		</AbsoluteFill>
 	);
