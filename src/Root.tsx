78a0a5065e572af93ff21cd962e3aba209ba5484
George Satellite <lunaerxs@gmail.com>
2023-06-24 21:55:54 +0300

    use eslint CLI instead of xo

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,13 +2,15 @@ import {Composition as RemotionComposition} from 'remotion';
 import './style.css';
 import {Composition} from './Composition';
 
-export const Root = () => (
-	<RemotionComposition
-		id='Main'
-		component={Composition}
-		durationInFrames={60}
-		fps={30}
-		width={1280}
-		height={720}
-	/>
-);
+export const Root = () => {
+	return (
+		<RemotionComposition
+			id='Main'
+			component={Composition}
+			durationInFrames={60}
+			fps={30}
+			width={1280}
+			height={720}
+		/>
+	);
+};
