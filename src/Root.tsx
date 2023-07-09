a799ff70f4e3c52bb424519e7e6b975776c2b302
George Satellite <lunaerxs@gmail.com>
2023-07-10 00:24:46 +0300

    Root: Combination

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,99 +1,80 @@
 import './style.css';
 import {Composition, staticFile} from 'remotion';
-import {VideoHorizontal, VideoVertical} from './videos';
+import {VideoHorizontal, VideoVertical, type VideoProps} from './videos';
 
 export const Root = () => {
-	const fps30 = 30;
-	const fps60 = 60;
-	const durationInSeconds = 10;
-	const audioFile = staticFile('dna.mp3');
-	const coverFile = staticFile('dna.jpeg');
-	const artist = 'Kendrick Lamar';
-	const track = 'DNA.';
-	const textColor = '#d91a2a';
-	const backgroundColor = '#0d0d0d';
-	const waveformColor = textColor;
+	return (
+		<Combination
+			id='video'
+			durationInSeconds={10}
+			audioFile={staticFile('dna.mp3')}
+			audioStartFromInSeconds={0}
+			coverFile={staticFile('dna.jpeg')}
+			artist='Kendrick Lamar'
+			track='DNA.'
+			textColor='#d91a2a'
+			backgroundColor='#0d0d0d'
+			waveformColor='#d91a2a'
+		/>
+	);
+};
+
+type CombinationProps = Omit<VideoProps, 'audioStartFrom'> & {
+	audioStartFromInSeconds: number;
+	id: string;
+	durationInSeconds: number;
+};
+
+const Combination = ({
+	audioStartFromInSeconds,
+	id,
+	durationInSeconds,
+	...restProps
+}: CombinationProps) => {
+	const resolveForFPS = (fps: number) => {
+		return {
+			fps,
+			durationInFrames: durationInSeconds * fps,
+			defaultProps: {
+				...restProps,
+				audioStartFrom: audioStartFromInSeconds * fps,
+			},
+		};
+	};
+
+	/**
+	 * To make sure the compositions are 100% FPS & resolution independent, we include its low-res version here.
+	 * Additionally, it might help with development on slower computers.
+	 */
 	return (
 		<>
 			<Composition
-				id='video-horizontal-60fps-1920x1080'
+				id={`${id}-horizontal-60fps-1920x1080`}
 				component={VideoHorizontal}
-				fps={fps60}
-				durationInFrames={durationInSeconds * fps60}
 				width={1920}
 				height={1080}
-				defaultProps={{
-					audioFile,
-					audioStartFrom: 0 * fps60,
-					coverFile,
-					artist,
-					track,
-					textColor,
-					backgroundColor,
-					waveformColor,
-				}}
+				{...resolveForFPS(60)}
 			/>
-			{/*
-				To make sure the composition is 100% FPS & resolution independent, we include its low-res version here.
-				Additionally, it might help with development on slower computers.
-			*/}
 			<Composition
-				id='video-horizontal-30fps-960x540'
+				id={`${id}-horizontal-30fps-960x540`}
 				component={VideoHorizontal}
-				fps={fps30}
-				durationInFrames={durationInSeconds * fps30}
 				width={960}
 				height={540}
-				defaultProps={{
-					audioFile,
-					audioStartFrom: 0 * fps30,
-					coverFile,
-					artist,
-					track,
-					textColor,
-					backgroundColor,
-					waveformColor,
-				}}
+				{...resolveForFPS(30)}
 			/>
 			<Composition
-				id='video-vertical-60fps-1080x1920'
+				id={`${id}-vertical-60fps-1080x1920`}
 				component={VideoVertical}
-				durationInFrames={durationInSeconds * fps60}
-				fps={fps60}
 				width={1080}
 				height={1920}
-				defaultProps={{
-					audioFile,
-					audioStartFrom: 0 * fps60,
-					coverFile,
-					artist,
-					track,
-					textColor,
-					backgroundColor,
-					waveformColor,
-				}}
+				{...resolveForFPS(60)}
 			/>
-			{/*
-				To make sure the composition is 100% FPS & resolution independent, we include its low-res version here.
-				Additionally, it might help with development on slower computers.
-			*/}
 			<Composition
-				id='video-vertical-30fps-540x960'
+				id={`${id}-vertical-30fps-540x960`}
 				component={VideoVertical}
-				fps={fps30}
-				durationInFrames={durationInSeconds * fps30}
-				width={Math.round(1080 / 2)}
-				height={Math.round(1920 / 2)}
-				defaultProps={{
-					audioFile,
-					audioStartFrom: 0 * fps30,
-					coverFile,
-					artist,
-					track,
-					textColor,
-					backgroundColor,
-					waveformColor,
-				}}
+				width={540}
+				height={960}
+				{...resolveForFPS(30)}
 			/>
 		</>
 	);
--- src/videos/index.ts
+++ src/videos/index.ts
@@ -1,2 +1,3 @@
+export type {VideoProps} from './types';
 export {VideoHorizontal} from './VideoHorizontal';
 export {VideoVertical} from './VideoVertical';
