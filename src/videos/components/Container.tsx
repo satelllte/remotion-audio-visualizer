cb4496535a96f22ef46f7654e74f45f86690e802
George Satellite <lunaerxs@gmail.com>
2023-07-09 23:03:07 +0300

    Container: fade out fx

--- src/videos/components/Container.tsx
+++ src/videos/components/Container.tsx
@@ -1,4 +1,4 @@
-import {AbsoluteFill} from 'remotion';
+import {AbsoluteFill, Easing, interpolate, useCurrentFrame, useVideoConfig} from 'remotion';
 import {loadFont} from '@remotion/google-fonts/Inter';
 
 const {fontFamily} = loadFont();
@@ -12,6 +12,17 @@ export const Container = ({backgroundColor, children}: ContainerProps) => {
 	return (
 		<AbsoluteFill style={{fontFamily, backgroundColor}} className='bg-black text-white'>
 			{children}
+			<FadeOut/>
 		</AbsoluteFill>
+	);
+};
+
+const FadeOut = () => {
+	const frame = useCurrentFrame();
+	const {fps, durationInFrames: durationTotal} = useVideoConfig();
+	const durationFx = fps;
+	const opacity = interpolate(frame, [durationTotal - durationFx, durationTotal], [0, 1], {easing: Easing.linear, extrapolateLeft: 'clamp'});
+	return (
+		<AbsoluteFill className='bg-black' style={{opacity}}/>
 	);
 };
