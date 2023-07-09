41c0194f2f674de114dd840ea4c0e6e9b51b4c73
George Satellite <lunaerxs@gmail.com>
2023-07-10 00:02:38 +0300

    VideoHorizontal: new UI

--- src/videos/VideoHorizontal.tsx
+++ src/videos/VideoHorizontal.tsx
@@ -23,9 +23,9 @@ export const VideoHorizontal = ({
 				audioFile={audioFile}
 				audioStartFrom={audioStartFrom}
 				width={Math.round(width)}
-				height={Math.round(height * 0.5)}
+				height={Math.round(height * 0.8)}
 				color={waveformColor}
-				lineWidth={Math.round(height * 0.0005)}
+				lineWidth={Math.round(height * 0.0012)}
 			/>
 		);
 	};
@@ -33,8 +33,15 @@ export const VideoHorizontal = ({
 	return (
 		<>
 			<Container backgroundColor={backgroundColor}>
-				<div className='relative flex gap-[5%] p-[5%]'>
-					<Cover file={coverFile} size={coverSize}/>
+				<div className='absolute inset-0'>
+					<div className='absolute inset-x-0 top-0 translate-y-[-52%] opacity-80'>
+						{renderWaveform()}
+					</div>
+					<div className='absolute inset-x-0 bottom-0 translate-y-[52%] opacity-80'>
+						{renderWaveform()}
+					</div>
+				</div>
+				<div className='flex items-center justify-between gap-[5%] p-[5%]'>
 					<Title
 						artist={artist}
 						track={track}
@@ -42,9 +49,7 @@ export const VideoHorizontal = ({
 						trackFontSize={trackFontSize}
 						color={textColor}
 					/>
-					<div className='absolute inset-x-0 bottom-0'>
-						{renderWaveform()}
-					</div>
+					<Cover file={coverFile} size={coverSize}/>
 				</div>
 			</Container>
 			<AudioTrack file={audioFile} startFrom={audioStartFrom}/>
