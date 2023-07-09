120304520217907e19b42c8fd58b35866ccefcb6
George Satellite <lunaerxs@gmail.com>
2023-07-09 18:06:17 +0300

    two video variants

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,13 +4,23 @@ import {Composition} from './Composition';
 
 export const Root = () => {
 	return (
-		<RemotionComposition
-			id='Main'
-			component={Composition}
-			durationInFrames={60}
-			fps={30}
-			width={1280}
-			height={720}
-		/>
+		<>
+			<RemotionComposition
+				id='video-30fps-720p'
+				component={Composition}
+				durationInFrames={30 * 2}
+				fps={30}
+				width={1280}
+				height={720}
+			/>
+			<RemotionComposition
+				id='video-60fps-1080p'
+				component={Composition}
+				durationInFrames={60 * 2}
+				fps={60}
+				width={1920}
+				height={1080}
+			/>
+		</>
 	);
 };
